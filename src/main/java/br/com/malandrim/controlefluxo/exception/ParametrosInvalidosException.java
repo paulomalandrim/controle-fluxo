cdbd9f7fcc265fe3dedd6796df9e8fa1bc235ed2
paulomalandrim <paulo.malandrim@gmail.com>
2024-05-09 20:47:17 -0300

    commit final

---
+++ src/main/java/br/com/malandrim/controlefluxo/exception/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package br.com.malandrim.controlefluxo.exception;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String s) {
+        super(s);
+    }
+}
